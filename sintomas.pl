bca34781c7796fcbe6430ba841a2729645ff2fbb
Rafael Akiyoshi <rafah94@hotmail.com>
2016-05-06 15:01:18 -0300

    Criado regras para delegacao do diagnostico
        Signed-off-by: Macário Soares <macario.junior@gmail.com
        Signed-off-by: Emanuel Holanda <manusenac@hotmail.com>
        Signed-off-by: Gustavo Sabino <gustavo.sabino@hotmail.com>

--- sintomas.pl
+++ sintomas.pl
@@ -1,49 +1,104 @@
-# Modo de leitura sintomas ---     "Febre é sintoma de Zika"
+%Modo de leitura sintomas ---     "Febre é sintoma de Zika"
 
-sintoma(febre, zika).
-sintoma(dorCabeca, zika).
+sintoma(febre, zika, dengue, chikungunya).
+sintoma(dorCabeca, zika, dengue, chikungunya).
+sintoma(dorNoCorpo, zika, dengue, chikungunya).
+sintoma(fadiga, zika, dengue, chikungunya).
+sintoma(nauseas, dengue, chikungunya).
+sintoma(nauseas, dengue, chikungunya).
 sintoma(conjuntivite, zika).
-sintoma(fadiga, zika).
-sintoma(dorNoCorpo, zika).
-
-
-sintoma(febre, dengue).
-sintoma(dorCabeca, dengue).
-sintoma(fadiga, dengue).
-sintoma(nauseas, dengue).
-sintoma(dorNoCorpo, dengue).
-
-
-sintoma(febre, chikungunya).
-sintoma(dorCabeca, chikungunya).
-sintoma(fadiga, chikungunya).
-sintoma(nauseas, chikungunya).
-sintoma(dorNoCorpo, chikungunya).
 sintoma(hipersensibilidadeLuz, chikungunya).
 sintoma(feridasNaBoca, chikungunya).
 
 %Modo de leitura febre ---     "No Zika a Febre é baixa"
 febre(zika, baixa).
-febre(dengue, alta).
-febre(chikungunya,alta).
+febre(dengue,chikungunya, alta).
 
 %Modo de leitura dorCabeca ---     "No Zika a dor de cabeça é baixa"
-dorCabeca(zika, fraca).
+dorCabeca(zika,chikungunya, fraca).
 dorCabeca(dengue, forte).
-dorCabeca(chikungunya, fraca).
+
 
 %Modo de leitura dorNoCorpo ---     "No Zika a dorno corpo é nas articulacoes"
-dorNoCorpo(zika, articulacoes).
+dorNoCorpo(zika,chikungunya, articulacoes).
 dorNoCorpo(dengue, muscular).
-dorNoCorpo(chikungunya, articulacoes).
+
+probabilidade(zika,50).
+probabilidade(chikungunya,30).
+probabilidade(dengue,100).
+
+%diagnostico(paciente,zika).
+%diagnostico(paciente,chikungunya).
+%diagnostico(paciente,dengue).
+
+%Regras
+
+diagnostico(X,zika):- probabilidade(zika,Z), probabilidade(chikungunya,K), probabilidade(dengue,T), Z>K, Z>T.
+diagnostico(X,chikungunya):- probabilidade(chikungunya,Z), probabilidade(zika,K), probabilidade(dengue,T), Z>K, Z>T.
+diagnostico(X,dengue):- probabilidade(dengue,Z), probabilidade(chikungunya,K), probabilidade(zika,T), Z>K, Z>T.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 menu:-
-    write('menu'), nl,
-    write('1 Dengue'), nl,
+    write('Voce esta com febre?'), nl,
+    write('1 Sim'), nl,
+    write('2 Nao'), nl,
     read(X),
-    option(X),
+    optionFebre(X),
     X==0,!.
 
-option(0):- !.
-option(1):- write('lala'),nl,!.
-option(2):- !.
+optionFebre(0):- !.
+optionFebre(1):- write('lala'),nl,!.
+optionFebre(2):- cabecaMenu(),!.
+
+
+cabecaMenu:-
+    write('Voce esta com dor de cabeca?'), nl,
+    write('1 Sim'), nl,
+    write('2 Nao'), nl,
+    read(Y),
+    optionCabeca(Y),
+    Y==0,!.
+
+optionCabeca(0):- !.
+optionCabeca(1):- write('lala'),nl,!.
+
+
+corpoMenu:-
+    write('Voce esta com dor no corpo?'), nl,
+    write('1 Sim'), nl,
+    write('2 Nao'), nl,
+    read(Y),
+    optionCorpo(Y),
+    Y==0,!.
+
+optionCorpo(0):- !.
+optionCorpo(1):- write('lala'),nl,!.
+
+nauseaMenu:-
+    write('Voce esta com dor de cabeca?'), nl,
+    write('1 Sim'), nl,
+    write('2 Nao'), nl,
+    read(Y),
+    optionNause(Y),
+    Y==0,!.
+
+optionCabeca(0):- !.
+optionCabeca(1):- write('lala'),nl,!.
+
+

b29ecbdad99e579746b34fda70651d7b159a7cdf
Macário Soares <macario.junior@gmail.com>
2016-05-06 12:25:34 -0300

    Criação da base de conhecimentos inicial
    Signed-off-by: Macário Soares <macario.junior@gmail.com
    Signed-off-by: Rafael Akiyoshi <rafah94@hotmail.com>
    Signed-off-by: Emanuel Holanda <manusenac@hotmail.com>
    Signed-off-by: Gustavo Sabino <gustavo.sabino@hotmail.com>

---
+++ sintomas.pl
@@ -0,0 +1,38 @@
+# Modo de leitura sintomas ---     "Febre é sintoma de Zika"
+
+sintoma(febre, zika).
+sintoma(dorCabeca, zika).
+sintoma(conjuntivite, zika).
+sintoma(fadiga, zika).
+sintoma(dorNoCorpo, zika).
+
+
+sintoma(febre, dengue).
+sintoma(dorCabeca, dengue).
+sintoma(fadiga, dengue).
+sintoma(nauseas, dengue).
+sintoma(dorNoCorpo, dengue).
+
+
+sintoma(febre, chikungunya).
+sintoma(dorCabeca, chikungunya).
+sintoma(fadiga, chikungunya).
+sintoma(nauseas, chikungunya).
+sintoma(dorNoCorpo, chikungunya).
+sintoma(hipersensibilidadeLuz, chikungunya).
+sintoma(feridasNaBoca, chikungunya).
+
+# Modo de leitura febre ---     "No Zika a Febre é baixa"
+febre(zika, baixa)
+febre(dengue, alta)
+febre(chikungunya,alta)
+
+# Modo de leitura dorCabeca ---     "No Zika a dor de cabeça é baixa"
+dorCabeca(zika, fraca)
+dorCabeca(dengue, forte)
+dorCabeca(chikungunya, fraca)
+
+# Modo de leitura dorNoCorpo ---     "No Zika a dorno corpo é nas articulacoes"
+dorNoCorpo(zika, articulacoes)
+dorNoCorpo(dengue, muscular)
+dorNoCorpo(chikungunya, articulacoes)
